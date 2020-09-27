55a9fc9c9f93151f671449269644689414c2b0cf
Steven Calverley <steven@stevencalverley.com>
2020-09-27 20:45:52 +1000

    Update BaseButton class styling and add another button example

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -1,3 +1,7 @@
 <template>
-  <button class="px-4 py-2 bg-blue-600 rounded text-white"><slot>Submit</slot></button>
+  <button
+    class="w-full px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline active:bg-blue-700 transition ease-in-out duration-150"
+  >
+    <slot>Submit</slot>
+  </button>
 </template>
